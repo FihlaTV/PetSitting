0c7158685d47236ae81e3457ca7545467845f466
poireau4 <32650287+poireau4@users.noreply.github.com>
2017-11-18 11:31:18 +0100

    upload to show error message

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,17 +1,27 @@
 import { Component } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import {ActionSheetController, ActionSheet, NavController, NavParams, ToastController} from 'ionic-angular';
 import {AdvertListPage} from '../advert-list/advert-list';
+
+import {UserService} from '../../providers/user-service-rest';
 
 
 @Component({
     selector: 'page-profile',
     templateUrl: 'profile.html'
 })
+
 export class ProfilePage {
 
-  constructor(public navCtrl: NavController) {
-  }
+ user: any;
+
+    constructor(public actionSheetCtrl: ActionSheetController, public navCtrl: NavController, public navParams: NavParams, public UserService: UserService, public toastCtrl: ToastController) {
+        this.user = this.navParams.data;
+        UserService.findById(this.user.id).then(
+            user => this.user = user
+        );
+
+    }   
 
   updateUserInfo(){
   this.navCtrl.push(AdvertListPage);
---
+++ src/providers/user-service-rest.ts
@@ -0,0 +1,27 @@
+import {Injectable} from '@angular/core';
+import {Http, Headers, RequestOptions} from '@angular/http';
+import {SERVER_URL} from './config';
+import 'rxjs/Rx';
+
+let usersURL = SERVER_URL + 'api/users/';
+
+@Injectable()
+export class UserService {
+  
+    constructor(public http: Http) {
+        this.http = http;
+    }
+
+    findAll() {
+        return this.http.get(usersURL)
+            .map(res => res.json())
+            .toPromise();
+    }
+
+    findById(id) {
+        return this.http.get(usersURL + "id/" + id)
+            .map(res => res.json())
+            .toPromise();
+    }
+   
+}
