b0eaf345b967a34f30115027f7342b147e713d87
Deathcell <kevin.favre@hec.edu>
2017-11-25 20:49:22 +0100

    Created a hardcoded dashboard

--- src/pages/dashboard/dashboard.ts
+++ src/pages/dashboard/dashboard.ts
@@ -27,32 +27,28 @@ export class DashboardPage {
  
             type: 'bar',
             data: {
-                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                labels: ["Pet owners", "Pet sitters"],
                 datasets: [{
-                    label: '# of Votes',
-                    data: [12, 19, 3, 5, 2, 3],
-                    backgroundColor: [
+                    label: '# of Users',
+                    data: [19, 12],
+                    backgroundColor: [	
                         'rgba(255, 99, 132, 0.2)',
                         'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
                     ],
                     borderColor: [
                         'rgba(255,99,132,1)',
                         'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
                     ],
                     borderWidth: 1
                 }]
             },
             options: {
                 scales: {
+                	xAxes: [{
+                		stacked: true
+            			}],
                     yAxes: [{
+                        stacked: true,
                         ticks: {
                             beginAtZero:true
                         }
@@ -66,9 +62,9 @@ export class DashboardPage {
  
             type: 'doughnut',
             data: {
-                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                labels: ["Dog", "Cat", "Rodent", "Bird", "Fish", "Reptile"],
                 datasets: [{
-                    label: '# of Votes',
+                    label: '# of Pets',
                     data: [12, 19, 3, 5, 2, 3],
                     backgroundColor: [
                         'rgba(255, 99, 132, 0.2)',
@@ -95,10 +91,10 @@ export class DashboardPage {
  
             type: 'line',
             data: {
-                labels: ["January", "February", "March", "April", "May", "June", "July"],
+                labels: ["April", "May", "June", "July", "September", "October", "November"],
                 datasets: [
                     {
-                        label: "My First dataset",
+                        label: "# of Clicks",
                         fill: false,
                         lineTension: 0.1,
                         backgroundColor: "rgba(75,192,192,0.4)",
@@ -116,7 +112,7 @@ export class DashboardPage {
                         pointHoverBorderWidth: 2,
                         pointRadius: 1,
                         pointHitRadius: 10,
-                        data: [65, 59, 80, 81, 56, 55, 40],
+                        data: [5, 8, 15, 31, 24, 27, 40],
                         spanGaps: false,
                     }
                 ]
