24af4a3305a1e1a671a9b7ff0a9feb342e7c6f60
frediiiiii <fpinoteau64@gmail.com>
2017-11-27 00:17:19 +0100

    debug

--- src/pages/pet-user-list/pet-user-list.ts
+++ src/pages/pet-user-list/pet-user-list.ts
@@ -17,7 +17,9 @@ export class PetUserListPage {
 
       
    constructor(public navCtrl: NavController, public service: PetService, public config: Config) {
-        this.service.findByOwnerId("5a104f9cf053ae624c001662")
+        /*this.service.findByOwnerId("5a104f9cf053ae624c001662")
+            .then(data => this.pets = data);*/
+        this.service.findAll()
             .then(data => this.pets = data);
     }
 
