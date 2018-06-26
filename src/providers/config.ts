67a5970c043985d219c1f03467d2217a2c09572d
Thomas Fihla <fihlatv@gmail.com>
2018-06-26 00:46:48 +0000

    Update config.ts

--- src/providers/config.ts
+++ src/providers/config.ts
@@ -1,2 +1,2 @@
-export let SERVER_URL = "https://desolate-anchorage-50635.herokuapp.com/";
+export let SERVER_URL = "http://35.196.196.139:8100/";
 //export let SERVER_URL = "http://localhost:5000/";
